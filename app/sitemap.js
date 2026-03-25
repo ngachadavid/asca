289b377ee33397e87c2cb1456f803619b4dafc76
David Ngacha <davidngacha01@gmail.com>
2026-03-26 00:42:08 +0300

    Moved it to root directly

---
+++ app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.ascal.eu/',
+      lastModified: '2026-03-17',
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.ascal.eu/about',
+      lastModified: '2026-03-17',
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.ascal.eu/projects',
+      lastModified: '2026-03-17',
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.ascal.eu/testimonials',
+      lastModified: '2026-03-17',
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.ascal.eu/contact',
+      lastModified: '2026-03-17',
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://www.ascal.eu/donate',
+      lastModified: '2026-03-17',
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+  ]
+}
